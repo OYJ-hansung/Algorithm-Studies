03b12f7a69b664039b846f8f9ec743365a6632ac
OYJ-hansung <dhdudwn95@hansung.ac.kr>
2022-01-12 14:55:36 +0900

    개미전사

--- 나동빈_개미전사.py
+++ 나동빈_개미전사.py
@@ -2,13 +2,11 @@ import sys
 
 N = int(sys.stdin.readline())
 graph = list(map(int, sys.stdin.readline().split()))
-d = [0]*N
+dynamic_graph = [0]*N
 
-d[0] = graph[0]
-d[1] = max(graph[0], graph[1])
-
+dynamic_graph[0] = graph[1]
+dynamic_graph[1] = max(dynamic_graph[0], graph[1])
 for idx in range(3, N):
-    d[idx] = max(d[idx-1], d[idx-2] + graph[idx])
+    dynamic_graph[idx] = max(dynamic_graph[idx-1], dynamic_graph[idx-2]+graph[idx])
 
-print(d[N-1])
-
+print(dynamic_graph[N-1])
