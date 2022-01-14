27289483a88ce677a9dd798a7bbf2447a4c76d48
OYJ-hansung <dhdudwn95@hansung.ac.kr>
2022-01-14 18:27:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-a, b, c = 1, 2, 3
-maximation = max(a,b,c)
-print(maximation)
+num_list = [1, 5, 7, 15, 16, 22, 28, 29]
+
+def get_odd_num(num_list):
+    answer = []
+    for idx in num_list:
+            if idx%2!=0:
+                answer.append(idx)
+    return answer
+    
+print(get_odd_num(num_list))
