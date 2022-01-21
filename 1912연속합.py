e4fb5c1350d27b354ce7698971022ee92593a6a5
OYJ-hansung <dhdudwn95@hansung.ac.kr>
2022-01-21 15:27:32 +0900

    Update 1912연속합.py

--- 1912연속합.py
+++ 1912연속합.py
@@ -5,19 +5,26 @@ graph = list(map(int, sys.stdin.readline().split()))
 mem = [0]*100001
 mem[0] = graph[0]
 sum = 0
-k = 1
+flag = False
+
 if N >1:
-    for idx in range(k, N):
-        if mem[0] <0 and graph[idx]<0:
+    for idx in range(1, N):
+        if mem[0] <0 and graph[idx]<0 and flag == False:
+            mem[idx] = graph[idx]
             continue
-        if mem[0] <0 and graph[1]>0:
-            mem[1] = graph[1]
+        elif mem[idx-1]<0 and graph[idx]>0:
+            mem[idx] = graph[idx]
+            flag = True
             continue
+        
         sum = mem[idx-1] + graph[idx]
-        if sum >= 0:
+        if sum >= 0 or (sum<0 and graph[idx]<0):
             mem[idx] = mem[idx-1] + graph[idx]
         else:
             mem[idx] = graph[idx]
+    print(mem[:N])
     print(max(mem[:N]))
 else:
     print(mem[0])
+
+    
