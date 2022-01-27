7e24250e7967deda514ca7f02e94d4b10e508aa8
OYJ-hansung <dhdudwn95@hansung.ac.kr>
2022-01-28 01:02:12 +0900

    updates

--- 17140이차원배열과연산.py
+++ 17140이차원배열과연산.py
@@ -1,8 +1,6 @@
-from itertools import count
 import sys
 from bisect import bisect_left, bisect_right
 from pprint import pprint
-from unittest.util import _count_diff_hashable
 target_row, target_col, target_value = map(int, sys.stdin.readline().split())
 graph = [list(map(int, sys.stdin.readline().split())) for _ in range(3)]
 pprint(graph, width=20)
@@ -15,6 +13,7 @@ def countNum(num, row):
 
 
 temp = []
+answer = []
 while graph[target_row-1][target_col] != target_value:
     for row in graph:
         length = len(row) # 3
@@ -24,8 +23,11 @@ while graph[target_row-1][target_col] != target_value:
             temp.append([idx,countNum(idx, row)]) # [[1, 2], [2, 1]]
         temp.sort(key=lambda x: x[0])
         temp.sort(key=lambda x: x[1]) # [[2, 1][1, 2]]
-        print(temp)
+        answer.append(sum(temp, []))
+        temp.clear()
+        print(answer)
+        
     break
-
+print(answer)
 
 
