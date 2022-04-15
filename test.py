46e4d7bf05f2b20777d06b6301fd621fccfbb45e
OYJ-hansung <dhdudwn95@hansung.ac.kr>
2022-04-15 13:36:08 +0900

    updates

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
+import re
 import sys
 
-dict = dict(a=1, b=2)
-print(dict)
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+
+A.sort()
+B.sort(reverse=True)
+answer = 0
+for idx in range(N):
+    answer += A[idx]*B[idx]
+
+print(answer)
